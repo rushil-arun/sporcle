b2c708c5fa22dbd44e2bcdbf1bf909aa9a1cd1d6
rushil-arun <rushilarun@gmail.com>
2026-02-02 14:35:43 -0800

    Structure for a player

---
+++ server/game/player.go
@@ -0,0 +1,21 @@
+package game
+
+import (
+	"github.com/gorilla/websocket"
+)
+
+type Player struct {
+	Username   string          // identifies the player
+	Connection *websocket.Conn // WebSocket connection to the server (e.g. *websocket.Conn)
+	Color      string          // hex color, unique within the game
+	Code       string          // game code this player belongs to
+}
+
+func NewPlayer(username string, connection *websocket.Conn, color string, code string) *Player {
+	return &Player{
+		Username:   username,
+		Connection: nil,
+		Color:      color,
+		Code:       code,
+	}
+}
